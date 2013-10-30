b61b0d9488385e0487b16880a7780de59d7fa2f1
Maor Kern <maor13@meet.mit.edu>
2013-10-30 19:09:03 +0200

    divisors stuff

--- Lab2/divisors.py
+++ Lab2/divisors.py
@@ -1,5 +1,8 @@
+def divisors(n):
+	for x in range(1,n):
+		if n % x  == 0:
+			print x
 if __name__ == "__main__":
-	n = int(raw_input("Give me a number. Now.")
-	for x in range(n):
-		print x+1
+	n = int(raw_input("Give me a number. Now."))
+	divisors(n)
 
