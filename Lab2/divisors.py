545d9b1204a59858b7042baae69f2b47da87654c
Maor Kern <maor13@meet.mit.edu>
2013-11-13 19:53:22 +0200

    comprehensive lists or whatever its name is

--- Lab2/divisors.py
+++ Lab2/divisors.py
@@ -1,7 +1,7 @@
 def divisors(n):
-	for x in range(1,n):
-		if n % x  == 0:
-			print x
+	thelist = [x for x in range(1,n) if (n % x) == 0]	
+	for x in thelist:
+		print x
 if __name__ == "__main__":
 	n = int(raw_input("Give me a number. Now."))
 	divisors(n)
